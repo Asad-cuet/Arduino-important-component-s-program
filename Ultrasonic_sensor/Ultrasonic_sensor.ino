57963f217a7afcab5b5d7c34321e1089aea62f84
Asadul Islam Hamzah <u1911022@student.cuet.ac.bd>
2021-04-20 23:57:40 +0600

    1st commit

---
+++ Ultrasonic_sensor/Ultrasonic_sensor.ino
@@ -0,0 +1,24 @@
+int trig = 1;
+int echo = 2;
+float distance,duration;
+
+void setup() {
+    pinMode(trig, OUTPUT);
+    pinMode(echo, INPUT);
+    Serial.begin(9600);
+    
+}
+
+void loop() {
+
+  digitalWrite(trig, HIGH);
+  delay(0.01);
+  digitalWrite(trig, LOW);
+  duration =pulseIn(echo, HIGH); // duration=High hoye Low howa porjonto shomoi //
+  distance=duration*0.0332/2;
+  Serial.print("Distance=");
+  Serial.print(distance);
+  Serial.print("cm");
+  delay(500);
+  
+}
